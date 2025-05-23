06b8d6634f977a762b1ce23555a8eb005f0b63ac
sardorbek-3226 <ibrohimovs@gmail.com>
2025-05-23 09:01:48 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Info from "./components/Info"
-import About from "./components/about"
+import About from "./components/About"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
